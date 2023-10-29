2154561279ef40dd3874681738e4e1652a697d13
fancn21th <fancn21th@gmail.com>
2023-10-29 11:29:22 +0800

    👌 IMPROVE: 监听注入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
+import Stats from 'stats.js';
 import Foo from './components/foo/Foo.vue';
-import { onBeforeMount } from 'vue';
+import { onMounted, onBeforeMount, shallowRef, provide } from 'vue';
 import { designWidthPx, designHeightPx, designWidth, designHeight } from './configs/layerConfig';
 
 onBeforeMount(() => {
@@ -14,10 +15,39 @@ onBeforeMount(() => {
   document.documentElement.style.setProperty('--design-height', designHeightPx);
   document.documentElement.style.setProperty('--scale', scale);
 });
+
+const monitorRef = shallowRef(null);
+const stats = new Stats();
+
+provide('monitor', {
+  animate,
+  startAnimation: () => {
+    requestAnimationFrame(animate);
+  },
+});
+
+function animate(render) {
+  stats.begin();
+
+  // monitored code goes here
+  render();
+
+  stats.end();
+
+  requestAnimationFrame(animate);
+}
+
+onMounted(() => {
+  stats.showPanel(1); // 0: fps, 1: ms, 2: mb, 3+: custom
+  monitorRef.value.appendChild(stats.dom);
+});
 </script>
 
 <template>
-  <Foo />
+  <div class="container">
+    <Foo />
+    <div class="monitor" ref="monitorRef"></div>
+  </div>
 </template>
 
 <style>
@@ -29,7 +59,8 @@ onBeforeMount(() => {
 
 html,
 body,
-#app {
+#app,
+.container {
   width: 100%;
   height: 100%;
 }
